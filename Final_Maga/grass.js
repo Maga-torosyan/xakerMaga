0c9743d62a2f3c7e29bd17d59607f9349d8b87d4
Maga-torosyan <108349322+Maga-torosyan@users.noreply.github.com>
2023-02-25 23:50:59 +0300

    Update grass.js

--- Final_Maga/grass.js
+++ Final_Maga/grass.js
@@ -1,33 +1,9 @@
-class Grass {
+class Grass extends All {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.multiply = 0;
-    this.directions = [
-      [this.x - 1, this.y - 1],
-      [this.x, this.y - 1],
-      [this.x + 1, this.y - 1],
-      [this.x - 1, this.y],
-      [this.x + 1, this.y],
-      [this.x - 1, this.y + 1],
-      [this.x, this.y + 1],
-      [this.x + 1, this.y + 1]
-    ];
+  super(x,y)
+  this.energy = 8;
   }
-  chooseCell() {
-    let found = [];
-    for (let i = 0; i < this.directions.length; i++) {
-      let x = this.directions[i][0];
-      let y = this.directions[i][1];
-      if (x >= 0 && y >= 0 && x < matrix[0].length && y < matrix.length) {
-        if (matrix[y][x] === 0) {
-          found.push(this.directions[i])
-        }
-      }
-
-    }
-    return found;
-  }
+ 
 
   mul() {
     this.multiply++;
@@ -42,9 +18,7 @@ class Grass {
       this.multiply = 0;
     }
   }
-
-
-
-
+  
 
 }
+

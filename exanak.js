f00d2ad1f52868e920d1f50dab226e803141016a
Maga-torosyan <108349322+Maga-torosyan@users.noreply.github.com>
2023-02-26 01:07:47 +0300

    Create exanak.js

---
+++ exanak.js
@@ -0,0 +1,8 @@
+let wint =document.querySelector(".wint");
+let block =document.querySelector(".block")
+
+
+block.onmouseenter =function (){
+    dv.style.backgroundColor ="red"
+    console.log("enter")
+}

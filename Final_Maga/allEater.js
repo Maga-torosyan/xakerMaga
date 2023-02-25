bd7b1c4561971aabdf54e4571f6a328feb01ed97
Maga-torosyan <108349322+Maga-torosyan@users.noreply.github.com>
2023-02-25 23:48:24 +0300

    Update allEater.js

--- Final_Maga/allEater.js
+++ Final_Maga/allEater.js
@@ -1,38 +1,16 @@
-class AllEater {
+class AllEater extends All{
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    super(x,y)
     this.energy = 10;
-    this.directions = [];
+    
+  
   }
 
-  getNewCoordinates() {
-    this.directions = [
-      [this.x - 1, this.y - 1],
-      [this.x, this.y - 1],
-      [this.x + 1, this.y - 1],
-      [this.x - 1, this.y],
-      [this.x + 1, this.y],
-      [this.x - 1, this.y + 1],
-      [this.x, this.y + 1],
-      [this.x + 1, this.y + 1]
-    ];
-  }
+  
 
   chooseCell(grass, grassEater) {
-    let found = [];
-    this.getNewCoordinates();
-    for (let i = 0; i < this.directions.length; i++) {
-      let x = this.directions[i][0];
-      let y = this.directions[i][1];
-      if (x >= 0 && y >= 0 && x < matrix[0].length && y < matrix.length) {
-        if (matrix[y][x] === grass || matrix[y][x] === grassEater) {
-          found.push(this.directions[i])
-        }
-      }
-
-    }
-    return found;
+  this.getNewCoordinates();
+  return super.chooseCell(grass, grassEater);
   }
 
   eat() {

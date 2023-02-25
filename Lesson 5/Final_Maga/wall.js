36bc2159897fdd9aab878d1a25243208ff1f35a4
Maga-torosyan <108349322+Maga-torosyan@users.noreply.github.com>
2023-02-26 01:06:30 +0300

    Update wall.js

--- Lesson 5/Final_Maga/wall.js
+++ Lesson 5/Final_Maga/wall.js
@@ -4,18 +4,7 @@ class Wall extends All {
         this.multiply = 0;
        
     }
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
+  
 
     chooseCell(grassEater, allEater) {
         let found = [];
